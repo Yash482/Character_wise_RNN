b509a26a18dc9888e615891004f5ab421f9bf8ba
Yash Bansal <agarwaly482@gmail.com>
2020-08-24 17:58:56 +0530

    added build cell function, error removed

--- get_lstm.py
+++ get_lstm.py
@@ -1,13 +1,20 @@
 import tensorflow as tf
 
 def build_lstm(lstm_size, num_layers, n_seq, keep_prob):
-    #get lstm layer
-    lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
-    #get dropout layer
-    drop = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob = keep_prob)
+
+    ### Build the LSTM Cell
     
-    #stack up multiple layers
-    cell = tf.contrib.rnn.MultiRNNCell([drop]*num_layers)
+    def build_cell(lstm_size, keep_prob):
+        # Use a basic LSTM cell
+        lstm = tf.contrib.rnn.BasicLSTMCell(lstm_size)
+        
+        # Add dropout to the cell
+        drop = tf.contrib.rnn.DropoutWrapper(lstm, output_keep_prob=keep_prob)
+        return drop
+    
+    
+    # Stack up multiple LSTM layers, for deep learning
+    cell = tf.contrib.rnn.MultiRNNCell([build_cell(lstm_size, keep_prob) for _ in range(num_layers)])
     initial_state = cell.zero_state(n_seq, tf.float32)
     
-    return  cell, initial_state
+    return cell, initial_state
