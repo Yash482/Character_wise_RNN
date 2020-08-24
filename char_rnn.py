c47b124b3faee2a2aeaf82a97648575789296f23
Yash Bansal <agarwaly482@gmail.com>
2020-08-24 12:49:31 +0530

    model created

--- char_rnn.py
+++ char_rnn.py
@@ -40,8 +40,33 @@ x, y = next(batches)
 
 from get_inputs import build_inputs
 from get_lstm import build_lstm
+from get_output_and_loss import build_loss, build_optimizer, build_output
 
-
+#Building our model
+class CharRNN:
+    def __init__(self, num_classes, n_seq= 64, seq_len= 50, lstm_size= 128, num_layers= 2, lr= 0.001, grad_clip= 5, sampling= False):
+        if sampling== True:
+            n_seq, seq_len = 1,1
+        else:
+            n_seq, seq_len = n_seq, seq_len
+        self.inputs, self.targets, self.keep_prob = build_inputs(n_seq, seq_len)
+        cell, self.initial_state = build_lstm(lstm_size, num_layers, n_seq, self.keep_prob)
+        
+        #Run data thru RNN layers
+        
+        #one hot inputs
+        x_one_hot = tf.one_hot(self.inputs, num_classes)
+        #collect seq of outputs from lstm
+        outputs, state = tf.nn.dynamic_rnn(cell, x_one_hot, initial_state = self.initial_state)
+        self.final_state = state
+        
+        #get softmax prediction and logits
+        self.prediction, self.logits = build_output(outputs, in_size = lstm_size, out_size = num_classes)
+        
+        #get loss and optimizer
+        self.loss = build_loss(self.logits, self.targets, lstm_size, num_classes)
+        self.optimizer = build_optimizer(self.loss, lr, grad_clip)
+        
 
 
 
