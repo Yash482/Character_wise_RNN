55e9bb0cf6d19e5b821d38bd90966ecffeffcf94
Yash Bansal <agarwaly482@gmail.com>
2020-08-24 17:59:47 +0530

    error removed in loss function

--- get_output_and_loss.py
+++ get_output_and_loss.py
@@ -12,26 +12,33 @@ def build_optimizer(loss, lr, grad_clip):
 
 
 def build_output(lstm_output, in_size, out_size):
-    seq_output = tf.concat(lstm_output, axis = 1)
+
+    # Reshape output so it's a bunch of rows, one row for each step for each sequence.
+    # That is, the shape should be batch_size*num_steps rows by lstm_size columns
+    seq_output = tf.concat(lstm_output, axis=1)
     x = tf.reshape(seq_output, [-1, in_size])
     
-    #connect softmax
+    # Connect the RNN outputs to a softmax layer
     with tf.variable_scope('softmax'):
-        softmax_w = tf.Variable(tf.truncated_normal((in_size, out_size), stddev = 0.1))
+        softmax_w = tf.Variable(tf.truncated_normal((in_size, out_size), stddev=0.1))
         softmax_b = tf.Variable(tf.zeros(out_size))
-
-    logits = tf.matmul(x, softmax_w) + softmax_b
-    output = tf.nn.softmax(logits, name = 'predictions')
     
-    return output, logits
-
+    # Since output is a bunch of rows of RNN cell outputs, logits will be a bunch
+    # of rows of logit outputs, one for each step and sequence
+    logits = tf.matmul(x, softmax_w) + softmax_b
+    
+    # Use softmax to get the probabilities for predicted characters
+    out = tf.nn.softmax(logits, name='predictions')
+    
+    return out, logits
 
 def build_loss(logits, targets, lstm_size, num_classes):
-    #calculate loss for targets and logits
+    
+    # One-hot encode targets and reshape to match logits, one row per batch_size per step
     y_one_hot = tf.one_hot(targets, num_classes)
     y_reshaped = tf.reshape(y_one_hot, logits.get_shape())
     
-    #softmax cross_entropy loss
-    loss= tf.nn.softmax_cross_entropy_with_logits(logits, labels = y_reshaped)
+    # Softmax cross entropy loss
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_reshaped)
     loss = tf.reduce_mean(loss)
-    return loss    
+    return loss
